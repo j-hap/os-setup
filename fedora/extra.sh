ada69082d34de9bc974b4af96bdf7b787910d961
j-hap <ju.hapke@gmail.com>
2026-03-27 15:18:02 +0100

    added flatpak install script

---
+++ fedora/extra.sh
@@ -0,0 +1,5 @@
+sudo dnf install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.signal.Signal
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub-beta org.freecad.FreeCAD
